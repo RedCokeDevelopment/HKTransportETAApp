b5c69720e85a47e16677fffdbd87644e2704c29d
Ian Lau <ianlau@Ians-MacBook-Air.local>
2022-09-26 13:56:09 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HK Transport Arrival Estimation',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
