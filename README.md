d8610c6104aea78770de8ebb7f4843c634bd11c3
ColaIan <colaian.yt@gmail.com>
2022-10-01 11:47:40 +0800

    Initialize Project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hong Kong Transport Arrival Estimation App
+
+An ionic side project to provide all-in-one Hong Kong transport arrival estimations from public data.
+
+```
+pnpm install
+npm i -g @ionic/cli
+ionic dev
+```
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'dev.redcoke.hktransporteta',
   appName: 'HK Transport Arrival Estimation',
   webDir: 'build',
   bundledWebRuntime: false
